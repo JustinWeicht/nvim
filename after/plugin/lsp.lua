a0cc4292a8c84570be5b258ba2e8d278ca5b8833
Justin Weicht <justinweicht11@gmail.com>
2024-06-03 13:17:50 -0400

    Add files via upload

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,35 @@
 local lsp_zero = require('lsp-zero')
 
+lsp_zero.set_preferences({
+    suggest_lsp_config = true,
+    sign_icons = {
+        error = 'E',
+        warning = 'W',
+        hint = 'H',
+        information = 'I'
+    }
+})
+
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+  local opt = { buffer = bufnr, remap = false }
+
+  vim.keymap.set('n', 'gd', function() vim.lsp_zero.buf.definition(opt) end, opts)
+  vim.keymap.set('n', 'K', function() vim.lsp_zero.buf.hover() end, opts)
+  vim.keymap.set('n', '<leader>vws', function() vim.lsp_zero.buf.workspace_symbol() end, opts)
+  vim.keymap.set('n', '<leader>vd', function() vim.lsp_zero.buf.open_float() end, opts)
+  vim.keymap.set('n', '[d', function() vim.lsp_zero.buf.goto_next() end, opts)
+  vim.keymap.set('n', ']d', function() vim.lsp_zero.buf.goto_prev() end, opts)
+  vim.keymap.set('n', '<leader>vca', function() vim.lsp_zero.buf.code_action() end, opts)
+  vim.keymap.set('n', '<leader>vrr', function() vim.lsp_zero.buf.references() end, opts)
+  vim.keymap.set('n', '<leader>vrn', function() vim.lsp_zero.buf.rename() end, opts)
+  vim.keymap.set('n', '<C-h>', function() vim.lsp_zero.buf.siganture_help() end, opts)
 end)
+
+lsp_zero.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
 
 local cmp = require('cmp')
 
