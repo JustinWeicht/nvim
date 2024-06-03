4debbb0f477c470fba594cbddfa408aedaba4c1a
Justin Weicht <justinweicht11@gmail.com>
2024-06-03 10:53:24 -0400

    Add files via upload

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,4 +1,21 @@
 require('refactoring').setup({})
 
-vim.api.nvim_set_keymap('n', '<leader>ri', [[ <Esc><cmd>lua require('refactoring').refactor('Inline variable')<CR>]], { noremap = true, silent = true })
+-- prompt for a refactor to apply when the remap is triggered
+vim.keymap.set(
+    {"n", "x"},
+    "<leader>rr",
+    function() require('refactoring').select_refactor() end
+)
+
+vim.keymap.set("x", "<leader>re", ":Refactor extract ")
+vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+vim.keymap.set( "n", "<leader>rI", ":Refactor inline_func")
+
+vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
 
